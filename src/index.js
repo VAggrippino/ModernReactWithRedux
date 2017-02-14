4a0e5377f5a1b241061f746f6102999531410023
Vince Aggrippino <ubuntu@ubuntu-xenial.localdomain>
2017-02-14 08:27:46 +0000

    First commit - Section 1 complete.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCvTWhGegliG4YgltFacyepL9Qvq29nHv4';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+// Create a new component. This component should produce some HTML.
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM);
+ReactDOM.render(<App />, document.querySelector('.container'));
